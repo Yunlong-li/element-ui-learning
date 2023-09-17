31702d546ab2a0a309688398d573a7fa7ccdf356
liuwei <lw97423@163.com>
2023-09-18 00:03:44 +0800

    文件上传下载

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--    <img alt="Vue logo" src="./assets/logo.png">-->
+    <!--    <BasicButton></BasicButton>-->
+    <!--    <Fileupload msg="Welcome to Your Vue.js App"/>-->
+    <!--    <Message></Message>-->
+    <!--    <Notification></Notification>-->
+    <!--    <Pagination></Pagination>-->
+        <ImageDownload></ImageDownload>
+    <!--    <CheckBox></CheckBox>-->
+<!--    <ImgDownload2></ImgDownload2>-->
+<!--    <MultiImageDownload></MultiImageDownload>-->
+<!--    <TravalNingBo></TravalNingBo>-->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Fileupload from "@/components/upload/Fileupload";
+import BasicButton from "@/components/Button/BasicButton";
+import Message from "@/components/notice/Message";
+import Notification from "@/components/notice/Notification";
+import Pagination from "@/components/pagination/Pagination";
+import CheckBox from "@/components/Table/CheckBox";
+import ImageDownload from "@/components/Test/ImageDownload";
+import ImgDownload2 from "@/components/Test/ImgDownload2";
+import MultiImageDownload from "@/components/Test/MultiImageDownload";
+import TravalNingBo from "@/components/Test/TravalNingBo";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BasicButton,
+    Fileupload,
+    Message,
+    Notification,
+    Pagination,
+    CheckBox,
+    ImageDownload,
+    ImgDownload2,
+    MultiImageDownload,
+    TravalNingBo
   }
 }
 </script>
@@ -24,5 +51,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  /*background-image: url(@/assets/NingBoCity_BCImage.jpeg);*/
+  /*background-repeat: repeat;*/
+  /*background-position: center;*/
+  /*background-attachment: scroll;*/
 }
+/*:root {*/
+/*  !*background-color: rgba(77, 77, 77, .5);*!*/
+
+/*}*/
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,32 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave : false,
+  devServer: {
+    // 第一种
+    // proxy:'http://localhost:8080'
+
+    // 第二种
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8099/',
+        pathRewrite: {
+          '^/api': ''
+        },
+        ws: true,  // 用于支持WebSocket
+        changeOrigin: true  // 用于控制请求头中的host值
+      },
+      // '/demo': {
+      //   target: 'http://localhost:5001',
+      //   pathRewrite: {
+      //     '^/demo': ''
+      //   },
+      //   ws: true,  // 用于支持WebSocket
+      //   changeOrigin: true  // 用于控制请求头中的host值
+      // },
+      // '/foo': {
+      //   target: '<other_url>'
+      // }
+    }
+  }
 })
